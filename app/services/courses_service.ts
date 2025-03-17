e6ab04363c1f7914ae5b6abbe23304e78d684b24
Anto' (SwartZ_) <seaflyscontact@gmail.com>
2025-03-17 13:48:35 +0100

    debug console log

--- app/services/courses_service.ts
+++ app/services/courses_service.ts
@@ -19,6 +19,7 @@ export default class CourseService {
       .where('id', id)
       .first()
 
+    console.log(course.articles.toString())
     // Convertir en JSON et inclure `quantity`
     return {
       ...course.toJSON(),
