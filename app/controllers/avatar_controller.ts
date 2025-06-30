db1a9456d5c29a9f3449fe477604e331b0832d86
SwartZCoding <seaflyscontact@gmail.com>
2025-06-30 13:08:27 +0200

    fix s3

--- app/controllers/avatar_controller.ts
+++ app/controllers/avatar_controller.ts
@@ -1,27 +1,36 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { inject } from '@adonisjs/core'
-import env from '#start/env'
-
-const s3 = new S3Client({
-  region: env.get('SCW_REGION'),
-  endpoint: env.get('SCW_ENPOINT'),
-  credentials: {
-    accessKeyId: env.get('SCW_ACCESS_KEY'),
-    secretAccessKey: env.get('SCW_SECRET_KEY'),
-  },
-})
+import { cuid } from '@adonisjs/core/helpers'
 
 @inject()
 export default class AvatarController {
   async update({ request, auth, response }: HttpContext) {
     const user = auth.user!
     const avatarFile = request.file('avatar', {
-      size: '2mb',
+      size: '4mb',
       extnames: ['jpg', 'png', 'jpeg'],
     })
 
     if (!avatarFile) {
       return response.badRequest({ message: 'Avatar file is required' })
+    }
+
+    if (!avatarFile.isValid) {
+      return response.badRequest({
+        message: 'Invalid file',
+        errors: avatarFile.errors,
+      })
+    }
+
+    const key = `uploads/${cuid()}.${avatarFile.extname}`
+    await avatarFile.moveToDisk(key)
+
+    user.avatar = avatarFile.meta.url
+    await user.save()
+
+    return {
+      message: 'Avatar updated successfully',
+      url: user.avatar,
     }
   }
 }
